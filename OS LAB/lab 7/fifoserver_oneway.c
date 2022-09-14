ff4f5b20cf3c80a2e8e51c0becbe4457978a22fc
Pulakesh5 <68358402+Pulakesh5@users.noreply.github.com>
2022-09-14 12:18:09 +0530

    Add files via upload

---
+++ OS LAB/lab 7/fifoserver_oneway.c
@@ -0,0 +1,33 @@
+/* Filename: fifoserver_oneway.c */
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+#define FIFO_FILE "MYFIFO"
+int main() {
+   int fd;
+   char readbuf[80];
+   char end[10];
+   int to_end;
+   int read_bytes;
+   
+   /* Create the FIFO if it does not exist */
+   mknod(FIFO_FILE,S_IFIFO|0640, 0);
+   strcpy(end, "end");
+   while(1) {
+      fd = open(FIFO_FILE, O_RDONLY);
+      read_bytes = read(fd, readbuf, sizeof(readbuf));
+      readbuf[read_bytes] = '\0';
+      to_end = strcmp(readbuf, end);
+      if (to_end == 0) {
+	printf("Communication ended\n");
+         close(fd);
+         break;
+      }
+      printf("Received string: \"%s\" and length is %d\n", readbuf, (int)strlen(readbuf));
+   }
+   return 0;
+}
